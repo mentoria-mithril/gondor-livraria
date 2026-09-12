6f5a203eb74e26d14e1fb9378d7183765013fc33
Joelson <joelsonsantos181040@gmail.com>
2026-09-12 16:16:51 -0300

    fix(carrinho): update user ID in obterCarrinhoUsuarioAtual function

--- api/src/controladores/carrinhoController.ts
+++ api/src/controladores/carrinhoController.ts
@@ -2,7 +2,7 @@ import type { Request,Response } from "express";
 import { obterCarrinhoUsuario } from "../servicos/carrinhoService.js";
 
 export async function obterCarrinhoUsuarioAtual(_req: Request,res: Response):Promise<void> {
-    const idUsuario = "231"; //TODO Trocar pelo id verdadeiro quando a fatia A 
+    const idUsuario = "19f72583-ba26-4e15-993c-7d2d5e848190"; //TODO Trocar pelo id verdadeiro quando a fatia A
     //de autenticação for terminada
     const carrinho = await obterCarrinhoUsuario(idUsuario);
     res.status(200).json(carrinho)
