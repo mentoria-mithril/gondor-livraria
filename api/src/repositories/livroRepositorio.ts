1f57cca53bb5d5ecdb551fa0681af05b9e9436b1
Lucca Sena <161384749+luccasena@users.noreply.github.com>
2026-09-13 15:56:58 -0300

    refactor: repository de livro;
    Para garantir que as consultas realizadas no banco de dados respeitem a regra de negócio definida para a busca de livros, foi necessária uma refatoração nas funções `buscarLivrosPorFiltros` e `quantidadeTotalLivrosPorFiltros`.
    
    Anteriormente, a presença de acentos no valor informado pelo usuário poderia influenciar o resultado da consulta.
    
    Para mitigar esse comportamento, foi adicionada a extensão `unaccent`. Com ela, os valores são normalizados durante a comparação, permitindo que a consulta desconsidere os acentos tanto no valor informado pelo usuário quanto nos dados armazenados nas colunas pesquisadas.

--- api/src/repositories/livroRepositorio.ts
+++ api/src/repositories/livroRepositorio.ts
@@ -1,74 +1,55 @@
 import { prisma } from "./prisma.js";
-import { LivroConsulta, LivroConsultaQuantidade, LivroConsultaDTO } from "../types/livro.type.js";
+import type { LivroConsulta, LivroConsultaQuantidade, LivroConsultaDTO } from "../types/livro.type.js";
 
-export function buscarLivrosPorFiltros({busca, categoria, pagina }: LivroConsulta): Promise<LivroConsultaDTO[]> {
-    return prisma.livro.findMany({
-        select: {
-            id: true,
-            titulo: true,
-            autor: true,
-            preco: true,
-            categoria: true,
-            estoque: true
-        },
-        where: {
-            ...(busca != null && busca !== "" && {
-                OR: [
-                    {
-                        autor: {
-                            contains: busca,
-                            mode: "insensitive"
-                        }
-                    },
-                    {
-                        titulo: {
-                            contains: busca,
-                            mode: "insensitive"
-                        }
-                    }
-                ]
-            }),
-            ...(categoria != null && categoria !== "" && {
-                categoria: {
-                    contains: categoria,
-                    mode: "insensitive"
-                }
-            })
-        },
-        skip: (pagina - 1) * 6,
-        take: 6,
-    }).then((livros) => livros.map((livro) => ({
-        ...livro,
-        preco: livro.preco.toString(),
-    })));
+export async function buscarLivrosPorFiltros({ busca, categoria, pagina }: LivroConsulta): Promise<LivroConsultaDTO[]> {
+    const buscaParam = busca?.trim() || null;
+    const categoriaParam = categoria?.trim() || null;
+    return await prisma.$queryRaw<LivroConsultaDTO[]>`
+        SELECT
+            l.id,
+            l.titulo,
+            l.autor,
+            l.preco,
+            l.categoria,
+            l.estoque
+        FROM livro l
+        WHERE
+            (
+                CAST(${buscaParam} AS TEXT) IS NULL
+                OR unaccent(l.titulo) ILIKE unaccent('%' || ${buscaParam} || '%')
+                OR unaccent(l.autor) ILIKE unaccent('%' || ${buscaParam} || '%')
+            )
+            AND
+            (
+                CAST(${categoriaParam} AS TEXT) IS NULL
+                OR unaccent(l.categoria) ILIKE unaccent('%' || ${categoriaParam} || '%')
+            )
+        ORDER BY l.id
+        LIMIT 6
+        OFFSET ${(pagina - 1) * 6};
+    `;
 }
 
-export function quantidadeTotalLivrosPorFiltros({ busca, categoria }: LivroConsultaQuantidade): Promise<number> {
-    return prisma.livro.count({
-        where: {
-            ...(busca != null && busca !== "" && {
-                OR: [
-                    {
-                        autor: {
-                            contains: busca,
-                            mode: "insensitive"
-                        }
-                    },
-                    {
-                        titulo: {
-                            contains: busca,
-                            mode: "insensitive"
-                        }
-                    }
-                ]
-            }),
+export async function quantidadeTotalLivrosPorFiltros({ busca, categoria }: LivroConsultaQuantidade): Promise<number> {
 
-            ...(categoria != null && categoria !== "" && {
-                categoria: {
-                    contains: categoria,
-                    mode: "insensitive"
-                }
-            })
-        }
-    });
+    const buscaParam = busca?.trim() || null;
+    const categoriaParam = categoria?.trim() || null;
+
+    const response = await prisma.$queryRaw<Array<{ count: bigint }>>`
+        SELECT COUNT(*) AS count
+        FROM livro l
+        WHERE
+            (
+                CAST(${buscaParam} AS TEXT) IS NULL
+                OR unaccent(l.titulo) ILIKE unaccent('%' || ${buscaParam} || '%')
+                OR unaccent(l.autor) ILIKE unaccent('%' || ${buscaParam} || '%')
+            )
+            AND
+            (
+                CAST(${categoriaParam} AS TEXT) IS NULL
+                OR unaccent(l.categoria) ILIKE unaccent('%' || ${categoriaParam} || '%')
+            );
+    `;
+
+    return Number(response[0]?.count ?? 0);
 }
